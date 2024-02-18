9658ccbc57e0183d877216ffe8b41cd77c00c951
BISWAJYOTI <biswajyoti.rout@thinktalentindia.com>
2024-02-18 18:45:15 +0530

    first commit

---
+++ Demo_Spring_Boot_App/src/main/java/com/example/demo_spring_boot_app/DemoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootAppApplication.class, args);
+    }
+
+}
